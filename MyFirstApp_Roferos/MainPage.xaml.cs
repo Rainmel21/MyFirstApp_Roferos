e8a3f29842b5ef4bd78aa2a302417f573e09994d
aceracad2 <aceracad2@DESKTOP-A1EI454>
2023-08-26 09:18:17 +0800

    Add project files.

---
+++ MyFirstApp_Roferos/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MyFirstApp_Roferos
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} stop the car";
+          
+            else
+                CounterBtn.Text = $"Clicked {count} tama na";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
---
+++ MyFirstApp_Roferos/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MyFirstApp_Roferos
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
